d622f7b57a7d4844b1b8c4a2052a436b8b63a6ba
mark-use-blender <markyoutub00@gmail.com>
2024-05-24 11:08:59 +0800

    bump

--- main.c
+++ main.c
@@ -20,12 +20,13 @@ openocd -f interface/raspberrypi-swd.cfg -f target/rp2040.cfg -c "program pico-s
 #include "spe2.pio.h"
 #include "src/pico_servo.h"
 #define SERVO_PIN 5
-#define estop 6
+#define estop_pin 6
+#define ARM_PIN 7
 
 
-void estop_callback(void) {
+void estop_pin_callback(uint gpio, uint32_t events) {
     // Stop the motor
-    servo_set_position(SERVO_PIN, 90);
+    servo_set_position(SERVO_PIN, 0);
     // Stop the PIO state machines
     pio_sm_set_enabled(pio0, 0, false);
     pio_sm_set_enabled(pio0, 1, false);
@@ -45,14 +46,23 @@ int main() {
     uint32_t off1, off2, spe1, spe2;
     uint32_t off1_last, off2_last, spe1_last, spe2_last;
     uint32_t offset, speeddiff;
-    int curpwm = 90;
+    uint32_t offset_last , speeddiff_last;
+    int curpwm = 0;
+    // Initialize estop pin
+    gpio_init(estop_pin);
+    gpio_set_dir(estop_pin, GPIO_IN);
+    gpio_pull_up(estop_pin);
     // Initialize interrupts
-    gpio_set_irq_enabled_with_callback (estop, GPIO_IRQ_EDGE_FALL, true, &estop_callback);
+    gpio_set_irq_enabled_with_callback (estop_pin, GPIO_IRQ_EDGE_FALL, true, &estop_pin_callback);
+    // Initialize arm pin
+    gpio_init(ARM_PIN);
+    gpio_set_dir(ARM_PIN, GPIO_IN);
+    gpio_pull_up(ARM_PIN);
     // Initialize PWM
     servo_enable(SERVO_PIN);
-    curpwm = 90;// put motor throttle to neutral position
+    curpwm = 0;// put motor throttle to neutral position
     servo_set_position(SERVO_PIN, curpwm); 
-
+    sleep_ms(500);
     // Initialize PIO
     PIO pio = pio0;
     // Get first free state machine in PIO 0
@@ -66,21 +76,23 @@ int main() {
     uint offset2 = pio_add_program(pio, &off2_program);
     uint offset3 = pio_add_program(pio, &spe1_program);
     uint offset4 = pio_add_program(pio, &spe2_program);
-
-
     // Initialize the program using the helper function in our .pio file
     off1_program_init(pio, sm1, offset1, 1);
     off2_program_init(pio, sm2, offset2, 1);
     spe1_program_init(pio, sm3, offset3, 1);
     spe2_program_init(pio, sm4, offset4, 1);
-
     // Start running our PIO program in the state machine
     pio_sm_set_enabled(pio, sm1, true);
     pio_sm_set_enabled(pio, sm2, true);
     pio_sm_set_enabled(pio, sm3, true);
     pio_sm_set_enabled(pio, sm4, true);
-    
-
+    //wait for arm cammand
+    while (gpio_get(ARM_PIN) == 0) {
+        sleep_ms(10);
+    }
+    curpwm = 30;// put motor throttle to start position
+    servo_set_position(SERVO_PIN, curpwm); 
+    sleep_ms(500);
     //match offset( min(off1,off2) smaller better) and speed (abs(spe1-spe2) smaller better)
     while (true) {
         //get the current value form the pio state machines
@@ -97,22 +109,26 @@ int main() {
         speeddiff = abs(spe1-spe2);
         offset = MIN(off1,off2);
         //adjust the motor power
-        if (abs(spe1_last-spe1) < 5 && abs(spe2_last-spe2) < 5 )
+        if (abs(speeddiff-speeddiff_last) < 50 && abs(offset-offset_last) < 50)
         {
             if (speeddiff > 10){
+                //speed adjustment
                 if (spe1 > spe2){
-                    curpwm = curpwm - 5;
+                    curpwm = curpwm - 1;
                 }else{  
-                    curpwm = curpwm + 5;
+                    curpwm = curpwm + 1;
                 }
+                curpwm = MIN(180,curpwm);
+                curpwm = MAX(30,curpwm);
                 servo_set_position(SERVO_PIN, curpwm);
             }else
             {
+                //offset adjustment
                 if (offset > 10){
                     if (off1 > off2){
-                        servo_set_position(SERVO_PIN, curpwm - 5);
+                        servo_set_position(SERVO_PIN, MAX(30 ,(curpwm - 1)));
                     }else{
-                        servo_set_position(SERVO_PIN, curpwm + 5);
+                        servo_set_position(SERVO_PIN, MIN(180,(curpwm + 1)));
                     }
                     sleep_ms(10);
                     servo_set_position(SERVO_PIN, curpwm );
@@ -129,6 +145,8 @@ int main() {
         off2_last = off2;
         spe1_last = spe1;
         spe2_last = spe2;
+        offset_last = offset;
+        speeddiff_last = speeddiff;
 
     }
 }
